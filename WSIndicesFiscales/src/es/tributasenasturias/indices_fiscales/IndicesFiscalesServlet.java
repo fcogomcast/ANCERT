de688faa46f0dd2828d0c063e43bc094fb10abaa
GROUPINFRA\f.gomezcastillejos <f.gomezcastillejos@cgi.com>
2024-02-28 16:55:49 +0100

    Modificar clase IndicesFiscalesServlet para compilación

--- WSIndicesFiscales/src/es/tributasenasturias/indices_fiscales/IndicesFiscalesServlet.java
+++ WSIndicesFiscales/src/es/tributasenasturias/indices_fiscales/IndicesFiscalesServlet.java
@@ -10,7 +10,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
-import weblogic.auddi.util.InitializationException;
+//import weblogic.auddi.util.InitializationException;
 
 
 import es.tributasenasturias.indices_fiscales.utils.Utils;
@@ -27,28 +27,28 @@ import es.tributasenasturias.indices_fiscales.utils.preferencias.PreferenciasFac
  * Servlet implementation class for Servlet: IndicesFiscalesServlet
  *
  */
- public class IndicesFiscalesServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
-   
+public class IndicesFiscalesServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
 
-/**
-	 * 
+
+	/**
+	 *
 	 */
 	private static final long serialVersionUID = 5047305406112553435L;
-static final String ENCODING="ISO-8859-1";
-   
-    /* (non-Java-doc)
+	static final String ENCODING="ISO-8859-1";
+
+	/* (non-Java-doc)
 	 * @see javax.servlet.http.HttpServlet#HttpServlet()
 	 */
 	public IndicesFiscalesServlet() {
 		super();
-	}   	
-	
+	}
+
 	/* (non-Java-doc)
 	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		throw new UnsupportedOperationException("No se pueden recibir peticiones mediante GET");
-	}  	
+	}
 	/**
 	 * Devuelve la petición al Servlet como un array de bytes
 	 * @param request {@link HttpServletRequest} con la petición al Servlet
@@ -77,9 +77,9 @@ static final String ENCODING="ISO-8859-1";
 	/**
 	 * Inicializa el contexto de la llamada
 	 * @return {@link CallContext} con los datos de Preferencias, Id de sesión y Log
-	 * @throws InitializationException
+	 * @throws IndicesFiscalesException
 	 */
-	private CallContext inicializarContext() throws InitializationException
+	private CallContext inicializarContext() throws IndicesFiscalesException
 	{
 		CallContext context = CallContextManager.newCallContext();
 		Preferencias pref=null;
@@ -88,12 +88,12 @@ static final String ENCODING="ISO-8859-1";
 			pref = PreferenciasFactory.newInstance();}
 		catch (PreferenciasException per)
 		{
-			throw new InitializationException(per, "Error en preferencias.");
+			throw new IndicesFiscalesException("Error en preferencias.",per);
 		}
 		String idSesion  = Utils.getIdLlamada();
 		if ("".equals(idSesion))
 		{
-			throw new InitializationException("No se puede recuperar el identificador de sesión.");
+			throw new IndicesFiscalesException("No se puede recuperar el identificador de sesión.");
 		}
 		Logger log = LogFactory.newLogger(pref.getModoLog(), pref.getFicheroLogAplicacion(), idSesion);
 		context.setItem(CallContextConstants.IDSESION, idSesion);
@@ -116,10 +116,10 @@ static final String ENCODING="ISO-8859-1";
 		//El fichero zip será pequeño (del orden de Ks), por lo que podemos tenerlo
 		//en memoria. Si no, sería mejor  trabajar con el stream mientras se pudiera.
 		CallContext context;
-		try 
+		try
 		{
 			context= inicializarContext();
-		} catch (InitializationException ce)
+		} catch (IndicesFiscalesException ce)
 		{
 			System.err.println("IndicesFiscales. Excepción en inicialización");
 			ce.printStackTrace(System.err);
@@ -128,7 +128,7 @@ static final String ENCODING="ISO-8859-1";
 		Logger log = (Logger) context.get(CallContextConstants.LOG);
 		log.info("Inicio de la recepción de fichero");
 		IndicesFiscalesImpl il = new  IndicesFiscalesImpl(context);
-		
+
 		try
 		{
 			//Log de fichero zip
@@ -154,5 +154,5 @@ static final String ENCODING="ISO-8859-1";
 				log.info("Fin de la recepción de fichero");
 			}
 		}
-	} 
+	}
 }
